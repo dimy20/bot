5a8b55851f93d44c4d0cf1156f42165474346e82
dimy20 <enzostrindade16@outlook.com>
2021-03-02 16:02:54 -0300

    oatuh response

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const axios = require("axios");
-
+const querystring = require("querystring");
 const redirect_uri = "http://localhost/api/v1";
 
 const discord_auth_url = `https://discord.com/api/oauth2/authorize?response_type=code&client_id=816071605386084353&scope=identify%20guilds.join&state=15773059ghq9183habn&redirect_uri=${redirect_uri}&prompt=consent`;
@@ -12,28 +12,31 @@ app.get("/", (req, res) => {
 
 app.get("/api/v1", async (req, res) => {
 	const code = req.query.code;
-	const state = req.query.state;
-	const API_ENDPOINT = "https://discord.com/api/v8";
-
+	const API_ENDPOINT = "https://discord.com/api/v8/oauth2/token";
 	const data = {
-		client_id: process.env.CLIENT_ID,
-		client_secret: process.env.CLIENT_SECRET,
-		grant_type: "authorization code",
+		client_id: "816071605386084353",
+		client_secret: "BFVhRDGDVF4_EO-wS-f_ZDPAjJ5KUc2H",
+		grant_type: "authorization_code",
 		code: code,
-		redirect_uri: "http://localhost/ap1/v1/done",
+		redirect_uri: "http://localhost/api/v1",
 		scope: "identify email connections",
 	};
 	const config = {
-		method: "POST",
+		method: "post",
 		url: API_ENDPOINT,
-		data: data,
+		data: querystring.stringify(data),
 		headers: {
 			"Content-Type": "application/x-www-form-urlencoded",
 		},
 	};
 
-	const response = await axios.post(config);
-	console.log(response);
+	try {
+		const response = await axios(config);
+		console.log(response.data);
+	} catch (error) {
+		console.log(error);
+	}
+
 	res.json({ message: "you are now authorized!" });
 });
 
