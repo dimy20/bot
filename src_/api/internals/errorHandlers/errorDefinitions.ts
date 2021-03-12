a3b02c8287ad006b01bf03d3501136b2cdc3a7eb
dimy20 <enzostrindade16@outlook.com>
2021-03-11 22:11:58 -0300

    no stress man

--- src_/api/internals/errorHandlers/errorDefinitions.ts
+++
@@ -1,8 +0,0 @@
-import {StatusCode,ErrorReason,ErrorType,Defaults} from "../constants/constants"
-
-export interface IERROR {
-    type : ErrorType, // change this to predefined values
-    statusCode : StatusCode,
-    reason: ErrorReason,
-    default :Defaults
-}
