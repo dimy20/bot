462c61559cbdec69c0d3c9d773af41f2acdfdc7d
dimy20 <enzostrindade16@outlook.com>
2021-03-16 15:40:20 -0300

    handkshake works

--- room/Socket.js
+++ room/Socket.js
@@ -1,12 +1,15 @@
-const stream = require("stream");
-const net = require("net");
-console.log("init");
-const socket = new net.Socket({ writable: true });
-
-socket.connect(1337,"127.0.0.1");
-socket.on("connection",(data)=>{console.log(data)});
-
-socket.on("data", (data) => {
-	const parsed = data.toString();
-	console.log(parsed);
+const WebSocket = require("ws");
+const ws = new WebSocket('ws://localhost',['json']);
+// Add a listener that will be triggered when the WebSocket is ready to use
+ws.addEventListener('open', () => {
+  const data = { message: 'Hello from the client!' }
+  const json = JSON.stringify(data);
+  // Send the message to the WebSocket server
+  ws.send(json);
+});
+// Add a listener in order to process WebSocket messages received from the server
+ws.addEventListener('message', event => {
+  // The `event` object is a typical DOM event object, and the message data sent
+  // by the server is stored in the `data` property
+  console.log('Received:', event.data);
 });
--- room/upgrade.js
+++ room/upgrade.js
@@ -2,8 +2,9 @@ const http = require("http");
 const crypto = require("crypto");
 
 const server = http.createServer((req,res)=>{
+/*
 const req_path = req.url;
-    const res_headers = handshake(req.headers);
+
     if(req_path === "/"){
             // Switching Protocols
             const statusCode = 101;
@@ -11,27 +12,57 @@ const req_path = req.url;
             //res.write("this is a test");
             res.end();
     }
+*/
 })
+
+/* 
+    -> Server receives masked data frames from the client.
+    -> The server MUST close the connection upon receiving a
+       frame that is not masked
+       In this case, a server MAY send a Close
+       frame with a status code of 1002 (protocol error)
+    -> A server MUST NOT mask any frames that it sends to
+       the client. 
+
+     -> The base framing protocol defines a frame type with an opcode, a
+        payload length, and designated locations for "Extension data" and
+        "Application data", which together define the "Payload data".
+
+    ->   A data frame MAY be transmitted by either the client or the server at
+        any time after opening handshake completion and before that endpoint
+        has sent a Close frame 
+*/
+server.on("upgrade",(req,res)=>{
+    if (req.headers['upgrade'] !== 'websocket') {
+    socket.end('HTTP/1.1 400 Bad Request');
+    return;
+    }
+    const res_headers = handshake(req.headers);
+    console.log(res_headers);
+    res.write(res_headers);
+
+})
+
+server.on("message",(data)=>{
+    console.log(data);
+})
+
 function handshake(headers){
     // RFC6455 guid value
     const guid = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"; 
     const sec_websocket_key = headers['sec-websocket-key'];
     // subprotocols the client supports
     const ws_protocols = headers['sec-websocket-protocol'].split(",");  
-    let chat = "";
+    let sub_protocol = ws_protocols.join(", ");
     // if chat is present as supported subprotocol by the client use it
+/*
     for(let p of ws_protocols)
-        if('chat' === p) chat = p;
+        if('json' === p) sub_protocol = p;*/
     // concats incoming sec-websocket-key header key to guid specified on RFC6455
     const concat_key_guid = sec_websocket_key + guid;
-    const sec_websocket_accept = crypto.createHash("SHA1").update(concat_key_guid).digest('base64');
-    const res_headers = {
-        Upgrade: 'websocket',
-        Connection: 'Upgrade',
-        'Sec-WebSocket-Accept':sec_websocket_accept,
-        'Sec-WebSocket-Protocol': chat // subprotocol to be used
-    }
-    return res_headers;
+    const accept = crypto.createHash("SHA1").update(concat_key_guid).digest('base64');
+    const response_headers = ['HTTP/1.1 101 Web Socket Protocol Handshake','Connection: Upgrade','Upgrade: WebSocket',`Sec-WebSocket-Accept: ${accept}`,`Sec-WebSocket-Protocol: ${sub_protocol}`];
+    return response_headers.join("\r\n") + "\r\n\r\n";
 }
     
 server.listen(80);
