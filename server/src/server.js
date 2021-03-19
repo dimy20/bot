2eedf2465b410838671adfa6ffa1c081daa11e7a
dimy20 <enzostrindade16@outlook.com>
2021-03-18 21:29:33 -0300

    nginx test passes

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const app = require("./api/gateway");
 const server = http.createServer(app);
-server.listen(80, () => {
+server.listen(process.env.PORT, () => {
 	console.log(`Server now running on port ${process.env.PORT}`);
 });
 
