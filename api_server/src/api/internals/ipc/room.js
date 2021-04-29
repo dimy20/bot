990d47bc8a6bdb0e1dacd3d3625a6a2be5587813
dimy20 <enzostrindade16@outlook.com>
2021-04-29 18:33:31 -0300

    room

--- api_server/src/api/internals/ipc/room.js
+++ api_server/src/api/internals/ipc/room.js
@@ -1,17 +1,18 @@
 const net = require("net");
 const {IPC_CREATE_ROOM} = require("../constants/constants");
 // this returns all the data related to the container creation if sucess
-async function createRoom(room,cb){
+async function createRoom(room){
     if(!room) return;
     return new Promise((resolve,reject)=>{
             const socket = net.connect({
                 port : 5000,
                 host : "room_service"
             })
+
             socket.on("connect",()=>{
-                    const room_json = {
-                        type : IPC_CREATE_ROOM,
-                        data : {
+                const room_json = {
+                    type : IPC_CREATE_ROOM,
+                    data : {
                             name: room.name,
                             expires:room.expiration,
                             max_connections: room.max_connections,
@@ -19,16 +20,17 @@ async function createRoom(room,cb){
                             pwd: room.pwd,
                             available : "next sunday"
                     }
-                }
-                    socket.write(JSON.stringify(room_json));
-                    
-
+                 }
+                socket.write(JSON.stringify(room_json));
             })
-            socket.on("data",(chunk)=>{
-                if(chunk.toString() === "") reject(new Error("Failed to create the room"));
-                socket.emit("close");
-                resolve(JSON.parse(chunk.toString()));
 
+            socket.on("data",(chunk)=>{
+                const res =  JSON.parse(chunk.toString());
+                if(res.success){
+                    socket.emit("close");
+                    resolve(res.data);
+                }
+                if(res.error) reject(res.error);
             })
     })
 
--- room_service/room_service.js
+++ room_service/room_service.js
@@ -7,35 +7,44 @@ const ROOM_EVENTS = {
     CREATE_ROOM : "CREATE_ROOM",
     GET_ROOM: "GET_ROOM"
 }
+class Response{
+    constructor(success,data,error){
+        this.success = success;
+        this.data = data;
+        this.error = error;
+    }
+}
 server.on("connection",(socket)=>{
     socket.on("data",(chunk)=>{
         const stream_data = JSON.parse(Buffer.from(chunk).toString());
         if(stream_data.type === ROOM_EVENTS.CREATE_ROOM){
+            //check if room exists
             const room = new room_model(stream_data.data);
-            room.save().then(res=>{
-                socket.write(JSON.stringify(res));
-            }).catch(err=>{
+            room
+            .save()
+            .then(res=>{
+                socket.write(JSON.stringify(new Response(true,res,null)));
+            })
+            .catch(err=>{
                 //notify error back to service requesting
-                console.log(err);
+                socket.write(JSON.stringify(new Response(false,null,err)));
             })
         }
         if(stream_data.type === ROOM_EVENTS.GET_ROOM){
-            room_model.findOne({name: stream_data.data.roomName}).then(result=>{
-                if(!result) socket.write(JSON.stringify(""));
+            room_model.findOne({name: stream_data.data.roomName})
+            .then(res=>{
+                if(!result) socket.write(JSON.stringify(new Response(true,res,null)));
                 else
-                    socket.write(JSON.stringify(result));
+                    socket.write(JSON.stringify(new Response(true,`room ${stream_data.data.roomName} was not found`,null)));
             }).catch(err=>{
                 console.log(err);
             })
         }
    })
-   socket.on("end",()=>{
-       console.log("endddd!");
-   })
-
-
     socket.on('end',()=>{console.log("connection closed")});
 });
 
 server.on("error",(error)=>{console.log(error)});
-server.listen(5000);
+server.listen(process.env.PORT,()=>{
+    `room service listening on port ${process.env.PORT}`
+});
